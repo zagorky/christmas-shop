67f4e4ab1b5cdc3f2f57f76bc4f741f5d4209dda
zagorky <zagorkiynahyi@gmail.com>
2024-11-01 19:33:23 +0300

    feat: make burger sceleton

--- christmas-shop/assets/scripts/burger.js
+++ christmas-shop/assets/scripts/burger.js
@@ -6,15 +6,35 @@ function toggleBurgerMenu() {
   if (window.innerWidth < 768) {
     burgerMenu.classList.toggle("open");
     toggleNav("open");
+    toggleScroll("open");
+  } else {
+    toggleScroll("open");
   }
 }
 
+function toggleScroll(classValue) {
+  if (burgerMenu.classList.contains(classValue)) {
+    document.body.classList.add("blocked");
+  } else {
+    document.body.classList.remove("blocked");
+  }
+}
 function toggleNav(classValue) {
-  if (classValue === "open") {
+  if (burgerMenu.classList.contains(classValue)) {
     navigation.classList.toggle("adaptiveNav");
+  } else {
+    navigation.classList.toggle("adaptiveNav");
+  }
+}
+
+function checkSize() {
+  if (window.innerWidth > 768) {
+    navigation.classList.remove("adaptiveNav");
+    burgerMenu.classList.remove("open");
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   burgerMenu.addEventListener("click", toggleBurgerMenu);
 });
+document.addEventListener("resize", checkSize);
