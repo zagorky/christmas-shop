9916c812bcf91556cd1fedce113a7796aa6b65fd
zagorky <zagorkiynahyi@gmail.com>
2024-11-01 13:45:23 +0300

    feat: publish branch, start doing burger, fix sloder width

--- christmas-shop/assets/scripts/burger.js
+++ christmas-shop/assets/scripts/burger.js
@@ -1,2 +1,17 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const burgerContainer = document.querySelector(".burger");
+const overlay = document.querySelector(".overlay");
+function toggleBurgerMenu() {
+  burgerMenu.classList.toggle("open");
+  toggleOverlay("open");
+}
+
+function toggleOverlay(value) {
+  if (value === "open") {
+    overlay.classList.toggle("open");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  burgerMenu.addEventListener("click", toggleBurgerMenu);
+});
