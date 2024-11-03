31074278f9ba17c56fe43c6d603cdedcb2e20c52
zagorky <zagorkiynahyi@gmail.com>
2024-11-03 21:54:19 +0300

    feat: slider is done

--- christmas-shop/assets/scripts/slider.js
+++ christmas-shop/assets/scripts/slider.js
@@ -1,10 +1,70 @@
 const slider = document.querySelector(".slider");
+const prevBtn = document.querySelector(".left");
+const nextBtn = document.querySelector(".right");
 
+const sliderWidth = slider.scrollWidth;
+let sliderVisibleWidth;
+let offset = 0;
+let currentOffset = 0;
+let click = 3;
+
+function setSliderVisibleWidth() {
+  return (sliderVisibleWidth = slider.clientWidth);
+}
+function calculateSliderReplacing() {
+  sliderVisibleWidth = setSliderVisibleWidth();
+  return (offset = (sliderWidth - sliderVisibleWidth) / changeNumbersOfClick());
+}
 function changeNumbersOfClick() {
-  if (window.innerWidth <= 768) {
-  } else if (window.innerWidth <= 380) {
+  return window.innerWidth >= 768 ? (click = 3) : (click = 6);
+}
+function moveSlider(offset) {
+  slider.style.transform = `translateX(-${offset}px)`;
+}
+function switchOffBnt() {
+  const maxOffset = sliderWidth - sliderVisibleWidth;
+
+  if (currentOffset <= 0) {
+    prevBtn.classList.add("inactiveNav");
+    prevBtn.classList.remove("activeNav");
   } else {
+    prevBtn.classList.add("activeNav");
+    prevBtn.classList.remove("inactiveNav");
+  }
+
+  if (currentOffset >= maxOffset) {
+    nextBtn.classList.add("inactiveNav");
+    nextBtn.classList.remove("activeNav");
+  } else {
+    nextBtn.classList.add("activeNav");
+    nextBtn.classList.remove("inactiveNav");
   }
 }
 
-window.addEventListener("resize", changeNumbersOfClick);
+window.addEventListener("resize", () => {
+  calculateSliderReplacing();
+  moveSlider(offset);
+  switchOffBnt();
+});
+
+prevBtn.addEventListener("click", () => {
+  calculateSliderReplacing();
+  if (currentOffset > 0) {
+    currentOffset -= offset;
+    moveSlider(currentOffset);
+    switchOffBnt();
+  }
+});
+
+nextBtn.addEventListener("click", () => {
+  calculateSliderReplacing();
+  if (currentOffset < sliderWidth - sliderVisibleWidth) {
+    currentOffset += offset;
+    moveSlider(currentOffset);
+    switchOffBnt();
+  }
+});
+
+setSliderVisibleWidth();
+calculateSliderReplacing();
+switchOffBnt();
