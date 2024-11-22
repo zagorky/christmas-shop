f70b124891f8ee4873d08796eaf88b9563e9157f
zagorky <zagorkiynahyi@gmail.com>
2024-11-22 22:56:52 +0300

    feat: try to deploy both pages

--- christmas-shop/vite.config.js
+++ christmas-shop/vite.config.js
@@ -1,3 +1,14 @@
-export default {
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   base: './',
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        gifts: resolve(__dirname, 'gifts/index.html'),
+      },
+    },
+  },
+});
