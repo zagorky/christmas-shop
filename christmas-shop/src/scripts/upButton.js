2218ebfdcb9c614072fbb5769a350466175a73f7
zagorky <zagorkiynahyi@gmail.com>
2024-11-29 23:25:12 +0300

    fix: change arrow elem

--- christmas-shop/src/scripts/upButton.js
+++ christmas-shop/src/scripts/upButton.js
@@ -4,7 +4,12 @@ const upButton = createElem({
   nodeElem: 'a',
   cssClasses: ['upButton'],
   attr: { href: '#top', title: 'Up' },
-  text: '\u{1f861}',
+  child: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 5V19" stroke="#FF4646" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M18 11L12 5" stroke="#FF4646" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M6 11L12 5" stroke="#FF4646" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`,
 });
 
 function createUpButton() {
@@ -17,7 +22,7 @@ function addUpButtonToPage() {
 }
 
 function checkScroll() {
-  const SCROLL_DOWN_OFFSET = 200;
+  const SCROLL_DOWN_OFFSET = 300;
   if (window.scrollY >= SCROLL_DOWN_OFFSET) {
     upButton.style.display = 'block';
   } else {
