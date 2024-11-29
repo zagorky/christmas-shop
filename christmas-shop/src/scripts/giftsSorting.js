e7d4e4a4446798cbc3c2f3285cda7da1b89c9c17
zagorky <zagorkiynahyi@gmail.com>
2024-11-29 23:26:16 +0300

    feat: delete slice

--- christmas-shop/src/scripts/giftsSorting.js
+++ christmas-shop/src/scripts/giftsSorting.js
@@ -27,7 +27,7 @@ function checkPage(gifts) {
   console.log('я проверка');
 
   if (window.location.href.includes('gifts')) {
-    return gifts.slice(0, 12); // убрать слайс перед 3 частью
+    return gifts;
   }
   shuffleGifts(gifts);
   return gifts.slice(0, 4);
